32dae0e26e0e01c70591cdb2527e3fbcf1a7b891
Jan Nahody <jan@nahody.com>
2015-05-05 15:41:00 +0200

    Merge new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,11 @@
 var gulp         = require('gulp'),
-    del          = require('del'),
     notify       = require('gulp-notify'),
     sass         = require('gulp-sass'),
     scssLint     = require('gulp-scss-lint'),
     rename       = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss    = require('gulp-minify-css'),
-    connect      = require('gulp-connect'),
-    gutil        = require('gulp-util'),
-    open         = require('gulp-open'),
-    gulpFilter   = require('gulp-filter');
+    gutil        = require('gulp-util');
 
 
 // Error notify handler
@@ -27,18 +23,11 @@ function notifyErrors() {
 }
 
 
-// TASK: clear
-gulp.task('clear', function () {
-    return del(['./docs/*.css']);
-});
-
-
 
 // TASK: sass
 gulp.task('sass', function () {
-    var filter = gulpFilter(['*yoso.*']);
 
-    return gulp.src(['./doc_styles/*.scss','./source/*.scss'])
+    return gulp.src(['./source/*.scss'])
         .pipe(scssLint({
             'config': './lint.yml'
         }))
@@ -54,51 +43,8 @@ gulp.task('sass', function () {
             dirname: '',
             extname: '.css'
         }))
-        .pipe(gulp.dest('./docs'))
-        .pipe(filter)
         .pipe(gulp.dest('./build'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest('./build'));
-});
-
-// TASK: http refresh
-gulp.task('http-refresh', function () {
-    return gulp.src('./docs/*.html').pipe(connect.reload());
-});
-
-gulp.task('watch', function () {
-    var logFunction = function (event) {
-        gutil.log(gutil.colors.green('File ' + event.path + ' was ' + event.type + ', running tasks...'));
-    };
-
-    // Create gulp watcher for scss files
-    gulp.watch(['./source/**/*','./doc_styles/**/*'], ['sass']).on('change', logFunction);
-
-    // Create gulp watcher for docs files
-    gulp.watch('./docs/**/*', ['http-refresh']).on('change', logFunction);
-});
-
-gulp.task('http', ['sass'], function () {
-    // config
-    var connectConfig = {
-        port: 8090,
-        livereload: {
-            ports: 35729
-        },
-        root: ['./docs']
-    };
-
-    // start http server
-    return connect.server(connectConfig);
-});
-
-// TASK: serve
-gulp.task('serve', ['sass', 'http', 'watch'], function () {
-
-    gulp.src('./docs/index.html')
-        .pipe(open('', {
-            url: 'http://localhost:8090',
-            app: 'Google Chrome'
-        }));
 });
